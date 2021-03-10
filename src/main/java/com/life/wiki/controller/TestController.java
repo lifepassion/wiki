af7ed3a9e888f83525d66dacc121c70c5842071e
life <1075794237@qq.com>
2021-03-10 22:44:01 +0800

    2-7 增加一个http测试接口

--- src/main/java/com/life/wiki/controller/TestController.java
+++ src/main/java/com/life/wiki/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.life.wiki.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping ("/hello/post")
+    public String hellop(String name){
+        return "Hello World "+name;
     }
 }
