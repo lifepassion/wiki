20a22bbb5c1bb37fd3610bf2d7e2914620657ba9
life <1075794237@qq.com>
2021-03-11 22:21:39 +0800

    2-8 增加yml文件 定义自定义配置项

--- src/main/java/com/life/wiki/controller/TestController.java
+++ src/main/java/com/life/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.life.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    String test;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World " + test;
     }
 
     @PostMapping ("/hello/post")
